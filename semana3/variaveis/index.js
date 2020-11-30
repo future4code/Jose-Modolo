89c2595d138c6af53138abad7150c3e3c792febe
Jose Modolo <josenetozxc@yahoo.com>
2020-11-30 16:33:36 -0300

    variaveis

---
+++ semana3/variaveis/index.js
@@ -0,0 +1,10 @@
+var nome = prompt ("Digite seu nome")
+var idade = prompt ("Digite sua idade")
+alert("Nome" + typeof nome)
+alert("Idade" + typeof idade)
+/*Fora impresso para o var nome e var idade o tipo String(estilo texto), uma var sem definição por padrão
+tem seu estilo String*/
+
+
+
+
