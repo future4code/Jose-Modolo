4f7815447c3afab6337313ab7bb16b19ded665e0
Jose Modolo <josenetozxc@yahoo.com>
2020-12-02 16:24:01 -0300

    projeto condicionais

---
+++ semana3/condicionais/exercicio 2/src/index.js
@@ -0,0 +1,16 @@
+/*Exercicio 2*/
+// a. Para que serve o codigo acima
+//É um catalogo de frutas onde o preço padrão de 5 reais.
+
+
+
+//b.Qual será a mensagem impressa no console, se o valor de fruta for "Maçã"?
+
+//"O preço da fruta maçã, é, R$2.25"
+
+
+
+//c. Considere que um usuário queira comprar uma Pêra, 
+//qual seria a mensagem impressa no console se retirássemos o break que está logo acima do default
+//(o break indicado pelo comentário "BREAK PARA O ITEM c.")?
+
---
+++ semana3/condicionais/exercicio 4/src/index.js
@@ -0,0 +1,10 @@
+//Exercicio 4 
+
+//Vamos criar programas que lidem com entradas do usuário! 
+//Faça um programa que pergunta ao usuário qual a idade dele e imprima no console se ele/ela pode dirigir 
+//(apenas maiores de idade).
+
+let idade = Number(prompt("Informe sua idade!"))
+if(idade >= 18){console.log("Você pode dirigir")}
+else{console.log("Você não pode dirigir")}
+
