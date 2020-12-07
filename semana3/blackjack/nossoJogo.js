8a56da5665c71170cff3cbf1893c65b395f595a8
Jose Modolo <josenetozxc@yahoo.com>
2020-12-06 22:59:49 -0300

    projeto blackjack

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -12,8 +12,26 @@
  */
 
  if(confirm("Quer iniciar uma nova rodada?")) {
-    console.log ("Bem vindo ao Blackjack!")
+   console.log ("Bem vindo ao Blackjack!")
+    
+   let jogadorCarta1 = comprarCarta()
+   let jogadorCarta2 = comprarCarta()
+   let computadorCarta1 = comprarCarta ()
+   let computadorCarta2 = comprarCarta ()
+   let jogadorPontuacao = jogadorCarta1.valor + jogadorCarta2.valor
+   let computadorPontuacao = computadorCarta1.valor + computadorCarta2.valor
+
+   console.log(`Jogador - cartas: ${jogadorCarta1.texto} ${jogadorCarta2.texto} - ${jogadorPontuacao}`)
+   console.log(`Computador - cartas: ${computadorCarta1.texto} ${computadorCarta2.texto} - ${computadorPontuacao}`)
+   if (jogadorPontuacao > computadorPontuacao) {
+      console.log("O jogador ganhou!")
+   } else if (computadorPontuacao > jogadorPontuacao) {
+      console.log("O computador ganhou!")
+   } else if (jogadorPontuacao === computadorPontuacao) {
+      console.log("Deu empate!")
+   } 
+    
  } else {
     console.log ("O jogo acabou")
-
+    alert ("GAME OVER")
  }
