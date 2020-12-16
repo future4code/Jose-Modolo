a449857d3927b8765310bf1177464db410fe06d5
Jose Modolo <josenetozxc@yahoo.com>
2020-12-16 15:46:18 -0300

    projeto insta

---
+++ semana5/componentes/insta4 2/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import Post from './components/Post/Post';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'app-container'}>
+        <Post
+          nomeUsuario={'Paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+          nomeUsuario={'Marcela'}
+          fotoUsuario={'https://i.picsum.photos/id/1023/3955/2094.jpg?hmac=AW_7mARdoPWuI7sr6SG8t-2fScyyewuNscwMWtQRawU'}
+          fotoPost={'https://picsum.photos/id/237/200/300'}
+        />
+
+        <Post
+          nomeUsuario={'João'}
+          fotoUsuario={'https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY'}
+          fotoPost={'https://i.picsum.photos/id/1004/5616/3744.jpg?hmac=Or7EJnz-ky5bsKa9_frdDcDCR9VhCP8kMnbZV6-WOrY'}
+        />
+      </div>
+      
+    );
+  }
+}
+
+export default App;
